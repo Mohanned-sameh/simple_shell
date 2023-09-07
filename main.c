1250902385155245028c6925c2830bc6a6c73c09
Mohanned <Mohanned2678@gmail.com>
2023-09-07 03:59:39 +0300

    starting simple shell

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char *usetext, *textcp, *delimiter = " \n";
+	char *strtoken, **av;
+	int i = 0, numoftokens = 0;
+	size_t num = 0;
+	ssize_t numchars;
+
+	printf("$ ");
+	numchars = getline();
+	return (0);
+}
