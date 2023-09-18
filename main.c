4acf6eef1d4c5a3e1ff0ff7258fd0440e8be141c
Mohanned-sameh <mohanned2678@gmail.com>
2023-09-18 19:30:07 +0300

    starting again

--- main.c
+++ main.c
@@ -1,71 +1,11 @@
 #include "main.h"
 /**
- * main - Entry point
+ * main - entry point
  * @ac: argument count
  * @av: argument vector
- * @env: environment variables
- * Return: Always 0 (Success)
+ * @env: environment
+ * Return: 0
  */
 int main(int ac, char **av, char **env)
 {
-	char *userline = NULL, *userlinecopy = NULL, *token = NULL,
-		 *delimiter = " \n";
-	size_t length = 0;
-	ssize_t userlineread;
-	int numoftokens = 0, i;
-
-	(void)ac;
-	while (1)
-	{
-		mywrite("$ ");
-		userlineread = getline(&userline, &length, stdin);
-		userlinecopy = malloc(sizeof(char) * userlineread);
-		checktext(userline, env, userlineread);
-		strcpy(userlinecopy, userline);
-		token = strtok(userline, delimiter);
-		while (token != NULL)
-		{
-			numoftokens++;
-			token = strtok(NULL, delimiter);
-		}
-		numoftokens++;
-		av = malloc(sizeof(char *) * numoftokens);
-		token = strtok(userlinecopy, delimiter);
-		for (i = 0; token != NULL; i++)
-		{
-			av[i] = malloc(sizeof(char) * strlen(token));
-			strcpy(av[i], token);
-			token = strtok(NULL, delimiter);
-		}
-		av[i] = NULL;
-		executecommand(av);
-	}
-	free(userlinecopy);
-	free(userline);
-	return (0);
-}
-/**
- * checktext - checks if the user input is valid
- * @text: user input
- * @env: environment variables
- * @userlineread: number of bytes read from stdin
- * Return: nothing
- */
-void checktext(char *text, char **env, ssize_t userlineread)
-{
-
-	if (userlineread == -1)
-	{
-		perror("Error: Unable to read from stdin\n");
-		exit(1);
-	}
-	if (strcmp(text, "exit\n") == 0)
-	{
-		mywrite("Goodbye!\n");
-		exit(1);
-	}
-	if (strcmp(text, "env\n") == 0)
-	{
-		print_environment_variables(env);
-	}
 }
--- main.h
+++ main.h
@@ -1,18 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#define BUFFER_SIZE 1024
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
-
-int mywrite(char *str);
-void executecommand(char **args);
-char *getuserpath(char *cmd);
-void print_environment_variables(char **environ);
-void checktext(char *text, char **env, ssize_t userlineread);
+#include <sys/stat.h>
 
 #endif
