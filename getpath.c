dd0dd06f6247c0ac6860544a1e1a181d94d5af9f
Mohanned-sameh <mohanned2678@gmail.com>
2023-09-20 01:05:02 +0300

    fixed betty

--- getpath.c
+++ getpath.c
@@ -6,39 +6,39 @@
  */
 char *getpath(char *cmd)
 {
-    char *path, *copyofpath, *pathoffile, *tokenofpath;
-    int cmdlen, dirlen;
-    struct stat st;
+	char *path, *copyofpath, *pathoffile, *tokenofpath;
+	int cmdlen, dirlen;
+	struct stat st;
 
-    path = getenv("PATH");
+	path = getenv("PATH");
 
-    if (path)
-    {
-        copyofpath = strdup(path);
-        cmdlen = strlen(cmd);
-        tokenofpath = strtok(copyofpath, ":");
-        while (tokenofpath)
-        {
-            dirlen = strlen(tokenofpath);
-            pathoffile = malloc(sizeof(char) * (dirlen + cmdlen + 2));
-            strcpy(pathoffile, tokenofpath);
-            strcat(pathoffile, "/");
-            strcat(pathoffile, cmd);
-            if (stat(pathoffile, &st) == 0)
-            {
-                free(copyofpath);
-                return (pathoffile);
-            }
-            else
-            {
-                free(pathoffile);
-                tokenofpath = strtok(NULL, ":");
-            }
-        }
-        free(copyofpath);
-        if (stat(cmd, &st) == 0)
-            return (cmd);
-        return (NULL);
-    }
-    return (NULL);
+	if (path)
+	{
+		copyofpath = strdup(path);
+		cmdlen = strlen(cmd);
+		tokenofpath = strtok(copyofpath, ":");
+		while (tokenofpath)
+		{
+			dirlen = strlen(tokenofpath);
+			pathoffile = malloc(sizeof(char) * (dirlen + cmdlen + 2));
+			strcpy(pathoffile, tokenofpath);
+			strcat(pathoffile, "/");
+			strcat(pathoffile, cmd);
+			if (stat(pathoffile, &st) == 0)
+			{
+				free(copyofpath);
+				return (pathoffile);
+			}
+			else
+			{
+				free(pathoffile);
+				tokenofpath = strtok(NULL, ":");
+			}
+		}
+		free(copyofpath);
+		if (stat(cmd, &st) == 0)
+			return (cmd);
+		return (NULL);
+	}
+	return (NULL);
 }
